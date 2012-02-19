0ee1f283a0c85c339fcd79a769bd30b0bcff0b27
gordon <wgordonw1@gmail.com>
2012-02-19 18:39:18 -0500

    create a function that will act as the module initiation point.  Named after runpy.run_module from http://docs.python.org/library/runpy.html#runpy.run_module
    Currently only sets __name__ and __builtins__.
    
    __name__ is set to "__main__" so that the standard if __name__ == "__main__" type code block can be used.
    
    __builtins__ is set to the pyjaco __builtins__ object.  Modules namespace __builtins__ so that it doesn't matter what the global builtin variable is named.

---
+++ pyjaco/stdlib/80-run_module.js
@@ -0,0 +1,8 @@
+$PY.run_module = function (mod_name, run_name) {
+	var __name__ = run_name || mod_name;
+	var kwargs = {
+		"__name__": str(__name__),
+		"__builtins__": __builtins__
+	};
+	$PY.modules[mod_name](__kwargs_make(kwargs));
+}
