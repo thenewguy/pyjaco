be30110d44f074f351ed684ef7a8423389fe9fb6
gordon <wgordonw1@gmail.com>
2012-07-18 21:15:24 -0400

    output useful information while generating QUnit tests

--- run_tests.py
+++ run_tests.py
@@ -179,103 +179,150 @@ def main():
         results = runner.run(test_suites)
     except KeyboardInterrupt:
         pass
-    if options.qunit:
-        heading = 'Creating QUnit Tests'
-        print ""
-        print "~" * len(heading)
-        print heading
-        print "~" * len(heading)
-        print ""
-        output_dir = os.path.join(os.getcwd(), "QUnit/")
-        output_test_dir = os.path.join(output_dir, "tests/")
-        output_html_path = os.path.join(output_dir, "tests.html")
-        for path in (output_test_dir, output_html_path):
-            try:
-                if os.path.isdir(path):
-                    shutil.rmtree(path)
-                else:
-                    os.remove(path)
-            except:
-                pass
-        for path in (output_dir, output_test_dir):
-            try:
-                os.makedirs(path)
-            except:
-                pass
-        qunit_suites = []
-        for suite, name in testtools.tests.get_test_names_in_suite(test_suites):
-            if hasattr(suite, "templ"):
-                qunit_suites.append(suite)
-        script_tag = ""
-        count = len(qunit_suites)
-        i = 1
-        for suite in qunit_suites:
-            output_test_path = os.path.join(output_test_dir, "%d.js" % i)
-            with open(output_test_path, "wb") as fp:
-                try:
-                    py_out_path = os.path.join(os.getcwd(), suite.templ["py_out_path"])
-                except KeyError:
-                    stderr.write("Test '%s' cannot be tested with QUnit.\n" % suite)
-                    continue
-                try:
-                    py_js_path = os.path.join(os.getcwd(), suite.templ["js_path"])
-                except KeyError:
-                    stderr.write("Test '%s' cannot be tested with QUnit.\n" % suite.templ["py_out_path"])
-                    continue
-                
-                if "js_run_file" in suite.templ:
-                    py_js_run_file_path = os.path.join(os.getcwd(), suite.templ["js_run_file"])
-                else:
-                    py_js_run_file_path = ""
-
-                if not os.path.exists(py_out_path) or not os.path.exists(py_js_path) or (py_js_run_file_path and not os.path.exists(py_js_run_file_path)):
-                    stderr.write("Could not create QUnit test for '%s'.\n" % suite.templ["js_path"])
-                    if not os.path.exists(py_out_path):
-                        stderr.write("Python output file '%s' did not exist.\n" % py_out_path)
-                    if not os.path.exists(py_js_path):
-                        stderr.write("Compiled javascript file '%s' did not exist.\n" % py_js_path)
-                    if py_js_run_file_path and not os.path.exists(py_js_run_file_path):
-                        stderr.write("Javascript run file '%s' did not exist.\n" % py_js_run_file_path)
-                    stderr.write("\n")
-                    continue
-                
-                script_tag += '<script src="%s"></script>' % os.path.relpath(output_test_path, output_dir).replace("\\", "/")
-
-                write_to_qunit(fp, 'test("%s", function() {' % suite.templ["js_path"].replace("\\", "/"))
-                write_to_qunit(fp, 'var output = [];', 1)
-                write_to_qunit(fp, "var console = {};", 1)
-                write_to_qunit(fp, "console.log = function() {", 1)
-                write_to_qunit(fp, "var inputs = [];", 2)
-                write_to_qunit(fp, "for(var i = 0; i < arguments.length; i++) {", 2)
-                write_to_qunit(fp, "inputs[i] = ((arguments[i] != null) && arguments[i]._js_ !== undefined) ? str(arguments[i])._js_() : arguments[i];", 3)
-                write_to_qunit(fp, "}", 2)
-                write_to_qunit(fp, "output.push(((inputs.length === 0) ? '' : inputs.join(' ')));", 2)
-                write_to_qunit(fp, "}", 1)
-                write_to_qunit(fp, builtins, 1)
-                with open(py_js_path, "rb") as py_js:
-                    content = py_js.read().splitlines()
-                    if content:
-                        del content[0]# remove 'load("py-builtins.js");'
-                    write_to_qunit(fp, content, 1)
-                if getattr(suite, "imports", None):
-                    for imp in suite.imports:
-                        with open(imp, "rb") as imp_fp:
-                            write_to_qunit(fp, imp_fp.read(), 1)
-                if py_js_run_file_path:
-                    with open(py_js_run_file_path, "rb") as py_js_run_file:
-                        write_to_qunit(fp, py_js_run_file.read(), 1)
-                with open(py_out_path, "rb") as py_out:
-                    output = py_out.read()
-                output = output.replace("\\", "\\\\")
-                output = output.replace('"', '\\"')
-                output = output.splitlines()
-                output = "\\n".join(output)
-                write_to_qunit(fp, 'equal(output.join("\\n"), "%s");' % output, 1)
-                write_to_qunit(fp, "});")
-                stdout.write("QUnit test written: %d%% complete.\r" % (i / count * 100))
-                i += 1
-        with open(output_html_path, "wb") as fp:
-            output = """
+    if results and results.errors:
+        if options.qunit:
+            heading = 'Errors Encountered -- Skipped QUnit Tests!'
+            print ""
+            print "~" * len(heading)
+            print heading
+            print "~" * len(heading)
+            print ""
+            
+        if not options.no_error:
+            print
+            print "errors:"
+            print "  (use -x to skip this part)"
+            for test, error in results.errors:
+                print
+                print "*", str(test), "*"
+                print error
+    elif options.qunit:
+        if not results:
+            heading = 'No Results -- Skipped QUnit Tests!'
+            print ""
+            print "~" * len(heading)
+            print heading
+            print "~" * len(heading)
+            print ""
+        else:
+            if results.errors:
+                heading = 'Errors Encountered -- Skipped QUnit Tests!'
+                print ""
+                print "~" * len(heading)
+                print heading
+                print "~" * len(heading)
+                print ""
+            elif results.failures:
+                heading = 'Failures Encountered -- Skipped QUnit Tests!'
+                print ""
+                print "~" * len(heading)
+                print heading
+                print "~" * len(heading)
+                print ""
+            elif results.expectedFailures:
+                heading = 'Expected Failures Encountered -- Skipped QUnit Tests!'
+                print ""
+                print "~" * len(heading)
+                print heading
+                print "~" * len(heading)
+                print ""
+            else:
+                heading = 'Creating QUnit Tests'
+                print ""
+                print "~" * len(heading)
+                print heading
+                print "~" * len(heading)
+                print ""
+                output_dir = os.path.join(os.getcwd(), "QUnit/")
+                output_test_dir = os.path.join(output_dir, "tests/")
+                output_html_path = os.path.join(output_dir, "tests.html")
+                for path in (output_test_dir, output_html_path):
+                    try:
+                        if os.path.isdir(path):
+                            shutil.rmtree(path)
+                        else:
+                            os.remove(path)
+                    except:
+                        pass 
+                for path in (output_dir, output_test_dir):
+                    try:
+                        os.makedirs(path)
+                    except:
+                        pass
+                qunit_suites = []
+                for suite, name in testtools.tests.get_test_names_in_suite(test_suites):
+                    if hasattr(suite, "templ"):
+                        qunit_suites.append(suite)
+                script_tag = ""
+                count = len(qunit_suites)
+                print "Writing %d QUnit test%s." % (count, "" if count == 1 else "s")
+                i = 1
+                for suite in qunit_suites:
+                    try:
+                        py_out_path = os.path.join(os.getcwd(), suite.templ["py_out_path"])
+                    except KeyError:
+                        stderr.write("Test '%s' cannot be tested with QUnit.\n" % suite)
+                        continue
+                    try:
+                        py_js_path = os.path.join(os.getcwd(), suite.templ["js_path"])
+                    except KeyError:
+                        stderr.write("Test '%s' cannot be tested with QUnit.\n" % suite.templ["py_out_path"])
+                        continue
+                    
+                    if "js_run_file" in suite.templ:
+                        py_js_run_file_path = os.path.join(os.getcwd(), suite.templ["js_run_file"])
+                    else:
+                        py_js_run_file_path = ""
+    
+                    if not os.path.exists(py_out_path) or not os.path.exists(py_js_path) or (py_js_run_file_path and not os.path.exists(py_js_run_file_path)):
+                        stderr.write("Could not create QUnit test for '%s'.\n" % suite.templ["js_path"])
+                        if not os.path.exists(py_out_path):
+                            stderr.write("Python output file '%s' did not exist.\n" % py_out_path)
+                        if not os.path.exists(py_js_path):
+                            stderr.write("Compiled javascript file '%s' did not exist.\n" % py_js_path)
+                        if py_js_run_file_path and not os.path.exists(py_js_run_file_path):
+                            stderr.write("Javascript run file '%s' did not exist.\n" % py_js_run_file_path)
+                        stderr.write("\n")
+                        continue
+                    
+                    output_test_path = os.path.join(output_test_dir, "%d.js" % i)
+                    with open(output_test_path, "wb") as fp:
+                        script_tag += '<script src="%s"></script>' % os.path.relpath(output_test_path, output_dir).replace("\\", "/")
+        
+                        write_to_qunit(fp, 'test("%s", function() {' % suite.templ["js_path"].replace("\\", "/"))
+                        write_to_qunit(fp, 'var output = [];', 1)
+                        write_to_qunit(fp, "var console = {};", 1)
+                        write_to_qunit(fp, "console.log = function() {", 1)
+                        write_to_qunit(fp, "var inputs = [];", 2)
+                        write_to_qunit(fp, "for(var i = 0; i < arguments.length; i++) {", 2)
+                        write_to_qunit(fp, "inputs[i] = ((arguments[i] != null) && arguments[i]._js_ !== undefined) ? str(arguments[i])._js_() : arguments[i];", 3)
+                        write_to_qunit(fp, "}", 2)
+                        write_to_qunit(fp, "output.push(((inputs.length === 0) ? '' : inputs.join(' ')));", 2)
+                        write_to_qunit(fp, "}", 1)
+                        write_to_qunit(fp, builtins, 1)
+                        with open(py_js_path, "rb") as py_js:
+                            content = py_js.read().splitlines()
+                        if content:
+                            del content[0]# remove 'load("py-builtins.js");'
+                        write_to_qunit(fp, content, 1)
+                        if getattr(suite, "imports", None):
+                            for imp in suite.imports:
+                                with open(imp, "rb") as imp_fp:
+                                    write_to_qunit(fp, imp_fp.read(), 1)
+                        if py_js_run_file_path:
+                            with open(py_js_run_file_path, "rb") as py_js_run_file:
+                                write_to_qunit(fp, py_js_run_file.read(), 1)
+                        with open(py_out_path, "rb") as py_out:
+                            output = py_out.read()
+                        output = output.replace("\\", "\\\\")
+                        output = output.replace('"', '\\"')
+                        output = output.splitlines()
+                        output = "\\n".join(output)
+                        write_to_qunit(fp, 'equal(output.join("\\n"), "%s");' % output, 1)
+                        write_to_qunit(fp, "});")
+                        stdout.write("QUnit tests written: %d%% complete.\r" % round(i / count * 100))
+                        i += 1
+                output = """
 <!DOCTYPE html>
 <html>
     <head>
@@ -291,16 +338,9 @@ def main():
     </body>
 </html>
             """ % (qunit_title, script_tag)
-            fp.write(output.strip())
-        stdout.write("QUnit test written: 100% complete.\n")
-    if not options.no_error and results and results.errors:
-        print
-        print "errors:"
-        print "  (use -x to skip this part)"
-        for test, error in results.errors:
-            print
-            print "*", str(test), "*"
-            print error
+                with open(output_html_path, "wb") as fp:
+                    fp.write(output.strip())
+                stdout.write("QUnit tests written: 100% complete.\n")
 
 if __name__ == "__main__":
     main()
